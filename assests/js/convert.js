82809ff6394c57feee66af69cbbd498f4ae5ed39
dofxo <kargat504@gmail.com>
2022-09-23 04:31:41 +0330

    optimized looping through queryselector arrays.

--- assests/js/convert.js
+++ assests/js/convert.js
@@ -21,46 +21,51 @@ show(0), show(1)
 // FAQ answers pop up/out START
 
 // faq bg color change
-const faqDiv = document.getElementsByClassName('faq-questions')
+const faqDiv = document.querySelectorAll('.faq-questions')
 let clicked = false
-for (let i = 0; i < faqDiv.length; i++) {
-    faqDiv[i].addEventListener('click', () => {
+faqDiv.forEach(item => {
+    item.addEventListener('click', () => {
         if (clicked === false) {
-            faqDiv[i].style.background = '#72768f31'
+            item.style.background = '#72768f31'
             clicked = true
         } else {
-            faqDiv[i].style.background = ''
+            item.style.background = ''
             clicked = false
         }
 
     })
-}
-// faq icon bg color change 
-const plusIconClass = document.getElementsByClassName('plus-icon')
-const aMenu = document.getElementsByClassName('a-menu')
-let colorChanged = false
-function IconAndBackgroundColorChange(indexNumber) {
 
-    faqDiv[indexNumber].addEventListener('click', () => {
-        if (colorChanged === false) {
-            plusIconClass[indexNumber].style.color = '#5376f6'
-            plusIconClass[indexNumber].classList.add('bxs-minus-circle')
-            plusIconClass[indexNumber].classList.remove('bxs-plus-circle')
-            aMenu[indexNumber].style.height = '100px'
-            aMenu[2].style.height = '30px'
-            colorChanged = true
-        }
-        else {
-            plusIconClass[indexNumber].style.color = '#72768F'
-            plusIconClass[indexNumber].classList.remove('bxs-minus-circle')
-            plusIconClass[indexNumber].classList.add('bxs-plus-circle')
-            aMenu[indexNumber].style.height = '0'
-            colorChanged = false
-        }
-    })
+})
+   
 
-}
-IconAndBackgroundColorChange(0), IconAndBackgroundColorChange(1), IconAndBackgroundColorChange(2), IconAndBackgroundColorChange(3)
+
+
+    // faq icon bg color change 
+    const plusIconClass = document.getElementsByClassName('plus-icon')
+    const aMenu = document.getElementsByClassName('a-menu')
+    let colorChanged = false
+    function IconAndBackgroundColorChange(indexNumber) {
+
+        faqDiv[indexNumber].addEventListener('click', () => {
+            if (colorChanged === false) {
+                plusIconClass[indexNumber].style.color = '#5376f6'
+                plusIconClass[indexNumber].classList.add('bxs-minus-circle')
+                plusIconClass[indexNumber].classList.remove('bxs-plus-circle')
+                aMenu[indexNumber].style.height = '100px'
+                aMenu[2].style.height = '30px'
+                colorChanged = true
+            }
+            else {
+                plusIconClass[indexNumber].style.color = '#72768F'
+                plusIconClass[indexNumber].classList.remove('bxs-minus-circle')
+                plusIconClass[indexNumber].classList.add('bxs-plus-circle')
+                aMenu[indexNumber].style.height = '0'
+                colorChanged = false
+            }
+        })
+
+    }
+    IconAndBackgroundColorChange(0), IconAndBackgroundColorChange(1), IconAndBackgroundColorChange(2), IconAndBackgroundColorChange(3)
 
 
 

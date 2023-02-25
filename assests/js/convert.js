0e2ca57b92b82e401ab84722976aa5a4840dcbff
Mohammad Kargar <57566624+dofxo@users.noreply.github.com>
2023-02-26 02:44:35 +0330

    fixed convert page FAQ section problem

--- assests/js/convert.js
+++ assests/js/convert.js
@@ -36,36 +36,35 @@ faqDiv.forEach(item => {
     })
 
 })
-   
 
 
 
-    // faq icon bg color change 
-    const plusIconClass = document.getElementsByClassName('plus-icon')
-    const aMenu = document.getElementsByClassName('a-menu')
-    let colorChanged = false
-    function IconAndBackgroundColorChange(indexNumber) {
 
-        faqDiv[indexNumber].addEventListener('click', () => {
-            if (colorChanged === false) {
-                plusIconClass[indexNumber].style.color = '#5376f6'
-                plusIconClass[indexNumber].classList.add('bxs-minus-circle')
-                plusIconClass[indexNumber].classList.remove('bxs-plus-circle')
-                aMenu[indexNumber].style.height = '100px'
-                aMenu[2].style.height = '30px'
-                colorChanged = true
-            }
-            else {
-                plusIconClass[indexNumber].style.color = '#72768F'
-                plusIconClass[indexNumber].classList.remove('bxs-minus-circle')
-                plusIconClass[indexNumber].classList.add('bxs-plus-circle')
-                aMenu[indexNumber].style.height = '0'
-                colorChanged = false
-            }
-        })
+// faq icon bg color change 
+const plusIconClass = document.getElementsByClassName('plus-icon')
+const aMenu = document.getElementsByClassName('a-menu')
+let colorChanged = false
+function IconAndBackgroundColorChange(indexNumber) {
 
-    }
-    IconAndBackgroundColorChange(0), IconAndBackgroundColorChange(1), IconAndBackgroundColorChange(2), IconAndBackgroundColorChange(3)
+    faqDiv[indexNumber].addEventListener('click', () => {
+        if (colorChanged === false) {
+            plusIconClass[indexNumber].style.color = '#5376f6'
+            plusIconClass[indexNumber].classList.add('bxs-minus-circle')
+            plusIconClass[indexNumber].classList.remove('bxs-plus-circle')
+            aMenu[indexNumber].style.height = '100px'
+            colorChanged = true
+        }
+        else {
+            plusIconClass[indexNumber].style.color = '#72768F'
+            plusIconClass[indexNumber].classList.remove('bxs-minus-circle')
+            plusIconClass[indexNumber].classList.add('bxs-plus-circle')
+            aMenu[indexNumber].style.height = '0'
+            colorChanged = false
+        }
+    })
+
+}
+IconAndBackgroundColorChange(0), IconAndBackgroundColorChange(1), IconAndBackgroundColorChange(2), IconAndBackgroundColorChange(3)
 
 
 
